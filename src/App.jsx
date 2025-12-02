78fc73d3ea30bfd4446dc49c4bb8c2531e0c533e
kloss254 <129971392+kloss254@users.noreply.github.com>
2025-12-02 14:14:32 +0300

    adding updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import GetInvolved from "./pages/GetInvolved";
 import Programs from "./pages/Programs";
+import Impact from "./pages/Impact";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/GetInvolved" element={<GetInvolved />} />
           <Route path="/Programs" element={<Programs />} />
+          <Route path="/impact" element={<Impact />} />
         </Routes>
       </div>
     </Router>
